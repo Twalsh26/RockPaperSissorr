18939f184d5e0bcf32854fd4c8b296f4e6d185e0
Thomas W <126Twalsh@frhsd.com>
2024-12-12 12:27:48 -0500

    For Ms

---
+++ js/java.js
@@ -0,0 +1,8 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('Players-choice')
+const resultDisplay = document.getElementById('Result')
+const possibleChoices = document.querySelectorAll('.card')
+let userChoice
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener ('click' , (e)=>{
+    console.log ("working")
+} ))
